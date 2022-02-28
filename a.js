b892f00a84fc4c56859fd87d3a45626919d4c05d
Shantnu Garg <gargshantnu@gmail.com>
2022-02-28 21:37:22 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,15 +1,17 @@
-// const result = require('./coverage/coverage-final.json');
-const result = require('./package.json');
+// const fileName = './coverage/coverage-final.json';
+const fileName = './package.json';
+
+const result = require(fileName);
 const fs = require("fs");
 
-console.log('a.js:', JSON.stringify(result.total));
+console.log('a.js:', JSON.stringify(result));
 fs.readdir('./', (err, files) => {
     files.forEach(file => {
         console.log(file);
     });
 });
 
-fs.readFile('./coverage/coverage-final.json', 'utf8', (err, data) => {
+fs.readFile(fileName, 'utf8', (err, data) => {
     if (err) {
         console.error(err)
         return
