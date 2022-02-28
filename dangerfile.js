e75597da88e6974e647a03e068b0bf0da7396c1e
Shantnu Garg <gargshantnu@gmail.com>
2022-02-28 21:56:57 +0530

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -134,7 +134,7 @@ jiraIssue({
 });
 
 
-const result = require('./coverage/coverage-final');
+const result = require('./coverage/coverage-final.json');
 const coverage = JSON.stringify(result.total)
 console.log("code: ", result);
 console.log("code coverage: ", coverage);
