b4479154bacb4fd3c286e9ebb71f8d678a28fcd4
Shantnu Garg <gargshantnu@gmail.com>
2022-02-28 20:41:07 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,7 +1,7 @@
 const result = require('./coverage/coverage-final.json');
 const fs = require("fs");
 
-console.log(JSON.stringify(result.total));
+console.log('a.js',JSON.stringify(result.total));
 fs.readdir('./', (err, files) => {
     files.forEach(file => {
         console.log(file);
