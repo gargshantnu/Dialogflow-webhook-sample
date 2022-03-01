92d78b5094bdb9b86f587d2365c6b458d0cf5c1f
Shantnu Garg <gargshantnu@gmail.com>
2022-03-01 16:48:57 +0530

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -104,11 +104,11 @@ const newCoverage = newCoverageReport.total;
 // const lineCoverage = `${"Lines".padEnd(10)} ${"?".padStart(10)} ${newCoverage.lines.pct.padStart(10)} ${"2".padStart(10)}\n`;
 // const statementCoverage = `${"Statement".padEnd(10)} ${"?".padStart(10)} ${newCoverage.statements.pct.padStart(10)} ${"2".padStart(10)}\n`;
 
-const functionCoverage = `${"Functions".padEnd(15)} ${newCoverage.functions.pct.padStart(15)}\n`;
-const lineCoverage = `${"Lines".padEnd(15)} ${newCoverage.lines.pct.padStart(15)}\n`;
-const statementCoverage = `${"Statement".padEnd(15)} ${newCoverage.statements.pct.padStart(15)}\n`;
+const functionCoverage = `${"Functions".padEnd(16)} ${newCoverage.functions.pct.padStart(16)}\n`;
+const lineCoverage = `${"Lines".padEnd(16)} ${newCoverage.lines.pct.padStart(16)}\n`;
+const statementCoverage = `${"Statement".padEnd(16)} ${newCoverage.statements.pct.padStart(16)}\n`;
 
-const prNumber = `#${danger.github.pr.number.toString()}`.padStart(15);
+const prNumber = `#${danger.github.pr.number.toString()}`.padStart(16);
 
 
 // const msg = "\n ```diff \n @@          Coverage Difference          @@"
@@ -119,10 +119,10 @@ const prNumber = `#${danger.github.pr.number.toString()}`.padStart(15);
 //   + " ```";
 
 const msg = "\n ```diff \n @@     Coverage Difference     @@"
-  + `\n ${"##".padEnd(14)} ${prNumber} \n========== ==========\n` 
+  + `\n ${"##".padEnd(15)} ${prNumber} \n============= =============\n` 
   + functionCoverage
   + lineCoverage
-  + statementCoverage + "========== ==========\n"
+  + statementCoverage + "============= =============\n"
   + "```";
 
 
