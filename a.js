0298e9f82f4dc7174c1c91a5342b9d4c58bd31e8
Shantnu Garg <gargshantnu@gmail.com>
2022-03-01 17:49:49 +0530

    Update a.js

--- a.js
+++ a.js
@@ -18,3 +18,37 @@ const fs = require("fs");
 //     }
 //     console.log("data: ", data);
 // })
+
+let newCoverageReport, masterCoverageReport;
+
+if (fs.existsSync('./coverage/coverage-summary.json')) {
+    newCoverageReport = require('./coverage/coverage-summary.json');
+}
+
+if (fs.existsSync('./coverage.json')) {
+    masterCoverageReport = require('./coverage.json');
+}
+
+const newCoverage = newCoverageReport?.total;
+const masterCoverage = masterCoverageReport?.total;
+
+console.log("newCoverage: ", newCoverage);
+console.log("masterCoverage: ", masterCoverage);
+// const functionCoverage = `${"Functions".padEnd(10)} ${"?".padStart(10)} ${newCoverage?.functions?.pct.padStart(10)} ${"2".padStart(10)}\n`;
+// const lineCoverage = `${"Lines".padEnd(10)} ${"?".padStart(10)} ${newCoverage?.lines?.pct.padStart(10)} ${"2".padStart(10)}\n`;
+// const statementCoverage = `${"Statement".padEnd(10)} ${"?".padStart(10)} ${newCoverage?.statements?.pct.padStart(10)} ${"2".padStart(10)}\n`;
+
+// const prNumber = `#${danger.github.pr.number.toString()}`.padStart(10);
+
+const functionCoverageDiff = `${(newCoverage?.functions?.pct - masterCoverage?.functions?.pct).toString()}`.padStart(10);
+// const prNumber = `#${danger.github.pr.number.toString()}`.padStart(10);
+// const prNumber = `#${danger.github.pr.number.toString()}`.padStart(10);
+
+// const msg = "\n ```diff \n @@          Coverage Difference          @@"
+//   + `\n ${"##".padEnd(9)} ${"master".padStart(10)} ${prNumber} ${"+/-".padStart(10)}\n========== ========== ========== ==========\n` 
+//   + functionCoverage
+//   + lineCoverage
+//   + statementCoverage + "========== ========== ========== ========== \n"
+//   + " ```";
+
+console.log("functionCoverageDiff: ", functionCoverageDiff)
